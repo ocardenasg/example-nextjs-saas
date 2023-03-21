f48357103f25a580207f947dd089db7684468135
Oscar Cárdenas <ojcardenas@outlook.com>
2023-03-20 21:40:21 -0600

    feature: create shared nav

---
+++ components/nav.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+
+import { useUser } from '@/context/user'
+
+export default function Nav() {
+  const user = useUser()
+  return (
+    <nav className="flex py-4 px-2 border-b boder-gray-200">
+      <Link href="/">Home</Link>
+      <Link href="/pricing" className="ml-2">
+        Pricing
+      </Link>
+      <Link href={(user && '/logout') || '/login'} className="ml-auto">
+        {(user && 'Logout') || 'Login'}
+      </Link>
+    </nav>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import { UserProvider } from '@/context/user'
+import Nav from '@/components/nav'
 
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
+      <Nav />
       <Component {...pageProps} />
     </UserProvider>
   )
