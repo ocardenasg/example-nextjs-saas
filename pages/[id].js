658e3dbc6900c071d538e6220c077239f51cdd7f
Oscar Cárdenas <ojcardenas@outlook.com>
2023-03-20 20:24:31 -0600

    feature: implement gated content using row level security un supabase

--- pages/[id].js
+++ pages/[id].js
@@ -1,10 +1,30 @@
+import { useState, useEffect, useCallback } from 'react'
+import Video from 'react-player'
+
 import { supabase } from '@/utils/supabase'
 
 export default function Lesson({ lesson }) {
+  const [videoUrl, setVideoUrl] = useState()
+
+  const getPremiumContent = useCallback(async () => {
+    const { data } = await supabase
+      .from('premium_content')
+      .select('video_url')
+      .eq('id', lesson.id)
+      .single()
+
+    setVideoUrl(data?.video_url)
+  }, [lesson.id])
+
+  useEffect(() => {
+    getPremiumContent()
+  }, [getPremiumContent])
+
   return (
     <main className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>{lesson.title}</h1>
       <p>{lesson.description}</p>
+      {!!videoUrl && <Video url={videoUrl} width="100%" height="100vh" />}
     </main>
   )
 }
